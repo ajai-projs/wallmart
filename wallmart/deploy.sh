18f26234a10f8fd51589ce1cb48233f59a0d3c3e
ajay0353 <ajay.paragodu@gmail.com>
2021-06-04 17:54:04 +0000

    updated from dev branch

--- wallmart/deploy.sh
+++ wallmart/deploy.sh
@@ -1,3 +1,3 @@
 Some changes are done through the development branch
 Hello shell scripts is my favourite language.
-
+now after commiting from dev branch i have switched to another branch and updated the code 

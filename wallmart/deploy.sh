ded1bfd345491e749122966259f2a502eceace7f
ajay0353 <ajay.paragodu@gmail.com>
2021-06-09 17:05:16 +0000

    git stash command

--- wallmart/deploy.sh
+++ wallmart/deploy.sh
@@ -1,5 +1,5 @@
 Some changes are done from the master branch
 Hello shell scripts is my favourite language.
 Im in master branch
-
+Here are some more changes used to get explore git stash 0--backup command
 now after commiting from dev branch i have switched to another branch and updated the code 
